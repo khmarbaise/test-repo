0f95ae6fac2b044115876989da7221903192d4b3
Karl-Heinz Marbaise <khmarbaise@innovas.de>
2015-06-01 09:58:12 +0200

    Nächster

--- test.c
+++ test.c
@@ -4,6 +4,10 @@ Hier geht es weiter
 Hier geht es weiter
 Hier geht es weiter
 Hier geht es weiter
+Hier ist die Änderung nach git add .
+Hier ist die Änderung nach git add .
+Hier ist die Änderung nach git add .
+Hier ist die Änderung nach git add .
 int main () {
   printf ("%f", 3.5d);
 }
