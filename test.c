9dada639c551017e09237895182ebab1112c49c1
Karl-Heinz Marbaise <khmarbaise@innovas.de>
2015-06-01 13:09:19 +0200

    New feature created

--- test.c
+++ test.c
@@ -8,9 +8,11 @@ Hier ist die Änderung nach git add .
 Hier ist die Änderung nach git add .
 Hier ist die Änderung nach git add .
 Hier ist die Änderung nach git add .
+Second
 new-branch
 new-branch
 new-branch
+second
 new-branch
 Change on Branch test-branch
 Change on Branch test-branch
